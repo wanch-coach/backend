ccc625bdabdc8a5101e47423134974797531f9e2
yuhojae <ho_0214@naver.com>
2024-05-21 00:14:50 +0900

    init commit

---
+++ wanchcoach/src/test/java/com/wanchcoach/WanchcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanchcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanchcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
