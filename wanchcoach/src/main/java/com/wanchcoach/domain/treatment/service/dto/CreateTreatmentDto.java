c9a56460930c29edb421dd436bcb32458936badf
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:34:56 +0900

    WC-711/chore: 불필요한 import 삭제

--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreateTreatmentDto.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreateTreatmentDto.java
@@ -1,12 +1,10 @@
 package com.wanchcoach.domain.treatment.service.dto;
 
-import com.wanchcoach.domain.treatment.entity.Treatment;
-
-public record CreateTreatmentDto(Long familyId, Long hospitalId, Long prescriptionId, String department,
+public record CreateTreatmentDto(Long familyId, Long hospitalId, String department,
                                  String date, Boolean taken, Boolean alarm, String symptom) {
 
-    public static CreateTreatmentDto of(Long familyId, Long hospitalId, Long prescriptionId, String department,
+    public static CreateTreatmentDto of(Long familyId, Long hospitalId, String department,
                                         String date, Boolean taken, Boolean alarm, String symptom) {
-        return new CreateTreatmentDto(familyId, hospitalId, prescriptionId, department, date, taken, alarm, symptom);
+        return new CreateTreatmentDto(familyId, hospitalId, department, date, taken, alarm, symptom);
     }
 }
