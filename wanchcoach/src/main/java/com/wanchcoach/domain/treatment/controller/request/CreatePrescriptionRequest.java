376813b756f52376fffa1b3bf0a0aaac0e5419c6
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:34:10 +0900

    WC-711/fix: 처방전 등록 시 입력값(가족 ID) 추가

--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/controller/request/CreatePrescriptionRequest.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/controller/request/CreatePrescriptionRequest.java
@@ -4,7 +4,7 @@ import com.wanchcoach.domain.treatment.service.dto.CreatePrescribedMedicineDto;
 
 import java.util.List;
 
-public record CreatePrescriptionRequest (Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
+public record CreatePrescriptionRequest (Long familyId, Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
                                          List<CreatePrescribedMedicineDto> prescribedMedicines) {
 
 }
--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreatePrescriptionDto.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreatePrescriptionDto.java
@@ -2,11 +2,11 @@ package com.wanchcoach.domain.treatment.service.dto;
 
 import java.util.List;
 
-public record CreatePrescriptionDto(Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
+public record CreatePrescriptionDto(Long familyId, Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
                                     List<CreatePrescribedMedicineDto> prescribedMedicines) {
 
-    public static CreatePrescriptionDto of(Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
+    public static CreatePrescriptionDto of(Long familyId, Long pharmacyId, Boolean morning, Boolean noon, Boolean evening, Boolean beforeBed,
                                            List<CreatePrescribedMedicineDto> prescribedMedicines) {
-        return new CreatePrescriptionDto(pharmacyId, morning, noon, evening, beforeBed, prescribedMedicines);
+        return new CreatePrescriptionDto(familyId, pharmacyId, morning, noon, evening, beforeBed, prescribedMedicines);
     }
 }
