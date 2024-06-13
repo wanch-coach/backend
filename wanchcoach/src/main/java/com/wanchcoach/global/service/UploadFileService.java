ef5aa03c033876545485932151d512a14a820490
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:44:00 +0900

    WC-711/chore: 불필요한 import 구문 삭제

--- wanchcoach/src/main/java/com/wanchcoach/global/service/UploadFileService.java
+++ wanchcoach/src/main/java/com/wanchcoach/global/service/UploadFileService.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.time.Instant;
 import java.util.Date;
