4a3512183ae6fd156b8353c5afb6852b8b57dd63
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:35:40 +0900

    WC-711/feat: 진료 등록 시 생성된 처방전 ID까지 반환하도록 로직 수정

--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/controller/response/CreateTreatmentResponse.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/controller/response/CreateTreatmentResponse.java
@@ -1,8 +1,8 @@
 package com.wanchcoach.domain.treatment.controller.response;
 
-public record CreateTreatmentResponse(Long treatmentId) {
+public record CreateTreatmentResponse(Long treatmentId, Long prescriptionId) {
 
-    public CreatePrescriptionResponse of(Long treatmentId) {
-        return new CreatePrescriptionResponse(treatmentId);
+    public CreateTreatmentResponse of(Long treatmentId, Long prescriptionId) {
+        return new CreateTreatmentResponse(treatmentId, prescriptionId);
     }
 }
