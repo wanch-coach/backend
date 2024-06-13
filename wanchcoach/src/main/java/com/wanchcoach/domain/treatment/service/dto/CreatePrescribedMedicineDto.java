6b72b1863c989184068c6b62602530417be342aa
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:33:04 +0900

    WC-711/rename: 약 엔티티명 변경(Medicine > Drug)

--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreatePrescribedMedicineDto.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/dto/CreatePrescribedMedicineDto.java
@@ -1,8 +1,8 @@
 package com.wanchcoach.domain.treatment.service.dto;
 
-public record CreatePrescribedMedicineDto(Long medicineId, Double Quantity, Integer frequency, Integer day, String direction) {
+public record CreatePrescribedMedicineDto(Long drugId, Double quantity, Integer frequency, Integer day, String direction) {
 
-    public static CreatePrescribedMedicineDto of(Long medicineId, Double Quantity, Integer frequency, Integer day, String direction) {
-        return new CreatePrescribedMedicineDto(medicineId, Quantity, frequency, day, direction);
+    public static CreatePrescribedMedicineDto of(Long drugId, Double quantity, Integer frequency, Integer day, String direction) {
+        return new CreatePrescribedMedicineDto(drugId, quantity, frequency, day, direction);
     }
 }
