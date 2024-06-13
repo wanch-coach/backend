942359df44120e0a02a7479b976639603bacef41
parkeg1223 <parkeg1223@gmail.com>
2024-06-13 22:41:18 +0900

    WC-711/feat: 진료 및 처방전 등록 기능 구현

--- wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/TreatmentService.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/treatment/service/TreatmentService.java
@@ -1,26 +1,31 @@
 package com.wanchcoach.domain.treatment.service;
 
+import com.wanchcoach.domain.family.repository.query.FamilyQueryRepository;
+import com.wanchcoach.domain.medical.repository.query.HospitalQueryRepository;
+import com.wanchcoach.domain.medical.repository.query.PharmacyQueryRepository;
+import com.wanchcoach.domain.drug.repository.query.DrugQueryRepository;
 import com.wanchcoach.domain.treatment.controller.response.CreatePrescriptionResponse;
 import com.wanchcoach.domain.treatment.controller.response.CreateTreatmentResponse;
-import com.wanchcoach.domain.treatment.entity.PrescribedMedicine;
+import com.wanchcoach.domain.treatment.entity.PrescribedDrug;
 import com.wanchcoach.domain.treatment.entity.Prescription;
 import com.wanchcoach.domain.treatment.entity.Treatment;
-import com.wanchcoach.domain.treatment.repository.command.PrescribedMedicineRepository;
+import com.wanchcoach.domain.treatment.repository.command.PrescribedDrugRepository;
 import com.wanchcoach.domain.treatment.repository.command.PrescriptionRepository;
 import com.wanchcoach.domain.treatment.repository.command.TreatmentRepository;
-import com.wanchcoach.domain.treatment.repository.query.PrescriptionQueryRepository;
 import com.wanchcoach.domain.treatment.repository.query.TreatmentQueryRepository;
 import com.wanchcoach.domain.treatment.service.dto.CreatePrescribedMedicineDto;
 import com.wanchcoach.domain.treatment.service.dto.CreatePrescriptionDto;
 import com.wanchcoach.domain.treatment.service.dto.CreateTreatmentDto;
+import com.wanchcoach.global.service.UploadFileService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 진료 서비스
@@ -36,10 +41,15 @@ import java.time.LocalDateTime;
 public class TreatmentService {
 
     private final TreatmentRepository treatmentRepository;
-    private final TreatmentQueryRepository treatmentQueryRepository;
     private final PrescriptionRepository prescriptionRepository;
-    private final PrescribedMedicineRepository prescribedMedicineRepository;
-    private final PrescriptionQueryRepository prescriptionQueryRepository;
+    private final PrescribedDrugRepository prescribedDrugRepository;
+    private final HospitalQueryRepository hospitalQueryRepository;
+    private final PharmacyQueryRepository pharmacyQueryRepository;
+    private final FamilyQueryRepository familyQueryRepository;
+    private final DrugQueryRepository drugQueryRepository;
+
+    private final UploadFileService uploadFileService;
+    private final TreatmentQueryRepository treatmentQueryRepository;
 
     /**
      * 진료 등록 API
@@ -52,9 +62,9 @@ public class TreatmentService {
         
         // 저장
         Treatment savedTreatment = treatmentRepository.save(Treatment.builder()
-                // todo: .family(FamilyQueryRepository.findById(dto.familyId())
-                .hospital(null) // todo: .hospital(.findById(dto.hospitalId())
-                .prescription(prescriptionQueryRepository.findById(dto.prescriptionId()))
+                .family(familyQueryRepository.findById(dto.familyId()))
+                .hospital(hospitalQueryRepository.findById(dto.hospitalId()))
+                .prescription(null)
                 .department(dto.department())
                 .date(LocalDateTime.parse(dto.date()))
                 .taken(dto.taken())
@@ -64,7 +74,7 @@ public class TreatmentService {
         );
 
         // 진료 id 반환
-        return new CreateTreatmentResponse(savedTreatment.getId());
+        return new CreateTreatmentResponse(savedTreatment.getId(), null);
     }
 
     /**
@@ -72,10 +82,7 @@ public class TreatmentService {
      *
      * @param dto 등록할 처방전(복용할 약) 정보
      */
-    public CreatePrescriptionResponse createPrescription(CreatePrescriptionDto dto, Long treatmentId, MultipartFile file) {
-        // todo: 계정 정보 - 가족 정보 연결 확인
-
-        // todo: 처방전 이미지 저장
+    public CreatePrescriptionResponse createPrescription(CreatePrescriptionDto dto, Long treatmentId, MultipartFile file) throws Exception {
 
         // 남은 복약 횟수 계산
         int maxRemains = 0;
@@ -85,28 +92,42 @@ public class TreatmentService {
 
         // 처방전 저장
         Prescription prescription = prescriptionRepository.save(Prescription.builder()
-                .pharmacy(null)         // todo: .pharmacy(pharmacyQueryRepository.findById(dto.pharmacyId())
+                .pharmacy(pharmacyQueryRepository.findById(dto.pharmacyId()))
                 .remains(maxRemains)    // todo: 전체 복약횟수 계산 로직 개선
                 .taking(true)
-                .endDate(LocalDate.now())
+                .endDate(null)
                 .build()
         );
 
+        // 진료 처방전 정보 저장
+        Treatment treatment = treatmentQueryRepository.findById(treatmentId);
+        treatment.updatePrescription(prescription);
+
+        // 처방전 이미지 저장
+        String fileName = familyQueryRepository.findNameById(dto.familyId()) +
+                "_" +
+                prescription.getId() +
+                "_" +
+                prescription.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) +
+                "." +
+                StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+        prescription.updateImageFileName(fileName);
+        fileName = "prescription/" + dto.familyId() + "/" + fileName;
+        uploadFileService.uploadPrescriptionImage(file, fileName);
+
         // 처방받은 약 저장
         for (CreatePrescribedMedicineDto cpmDto : dto.prescribedMedicines()) {
-            prescribedMedicineRepository.save(PrescribedMedicine.builder()
+            prescribedDrugRepository.save(PrescribedDrug.builder()
                     .prescription(prescription)
-                    .quantity(cpmDto.Quantity())
+                    .drug(drugQueryRepository.findById(cpmDto.drugId()))
+                    .quantity(cpmDto.quantity())
                     .frequency(cpmDto.frequency())
                     .day(cpmDto.day())
                     .direction(cpmDto.direction())
                     .build()
             );
         }
-
-        // 진료 정보에 저장
-        Treatment treatment = treatmentQueryRepository.findById(treatmentId);
-        treatment.updatePrescription(prescription);
 
         // todo: 예정된 복약 기록 저장
         /*
