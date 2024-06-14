2f7f38974a488dd410a09f37fe3c9a23002d6e3f
parkeg1223 <parkeg1223@gmail.com>
2024-06-15 05:42:56 +0900

    WC-715/feature: 가족 이름 조회 쿼리, 계정에 등록된 전체 가족 아이디 조회 쿼리 구현

--- wanchcoach/src/main/java/com/wanchcoach/domain/family/repository/query/FamilyQueryRepository.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/family/repository/query/FamilyQueryRepository.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.wanchcoach.domain.family.entity.QFamily.family;
 
 @Slf4j
@@ -25,15 +27,37 @@ public class FamilyQueryRepository {
     public Family findById(Long familyId) {
         return queryFactory
                 .selectFrom(family)
-                .where(family.id.eq(familyId))
+                .where(family.familyId.eq(familyId))
                 .fetchOne();
     }
 
+    /**
+     *
+     * 가족 ID로 이름 조회 쿼리
+     *
+     * @param familyId 가족 ID
+     * @return 해당 가족 이름
+     */
     public String findNameById(Long familyId) {
         return queryFactory
                 .select(family.name)
                 .from(family)
-                .where(family.id.eq(familyId))
+                .where(family.familyId.eq(familyId))
                 .fetchOne();
+    }
+
+    /**
+     *
+     * 회원 ID로 등록된 전체 가족 ID 조회
+     *
+     * @param memberId 회원 ID
+     * @return 해당 회원에 등록된 가족 ID 목록
+     */
+    public List<Long> findFamilyIdsByMemberId(Long memberId) {
+        return queryFactory
+                .selectDistinct(family.familyId)
+                .from(family)
+                .where(family.member.memberId.eq(memberId))
+                .fetch();
     }
 }
