57833de51178b71ce5e836cbdfbfe97132b5cbee
yuhojae <ho_0214@naver.com>
2024-06-21 00:58:41 +0900

    WC-691/fix : 소셜 로그인 시 회원 개인정보 데이터 추가 및 에러 수정

--- wanchcoach/src/main/java/com/wanchcoach/domain/auth/application/OAuthLoginService.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/auth/application/OAuthLoginService.java
@@ -1,5 +1,8 @@
 package com.wanchcoach.domain.auth.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.wanchcoach.domain.auth.infoResponse.OAuthInfoResponse;
 import com.wanchcoach.domain.auth.params.OAuthLoginParams;
 import com.wanchcoach.domain.auth.tokens.AuthTokenGenerator;
@@ -10,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +31,15 @@ public class OAuthLoginService {
         OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
         log.info(oAuthInfoResponse.toString());
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        try {
+            // 객체를 JSON 문자열로 변환
+            String json = objectMapper.writeValueAsString(oAuthInfoResponse);
+            System.out.println("test" + json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         String loginId = oAuthInfoResponse.getOAuthProvider() + oAuthInfoResponse.getEmail();
 
 
@@ -46,8 +60,18 @@ public class OAuthLoginService {
     private Member newMember(OAuthInfoResponse oAuthInfoResponse, String loginId){
         Member member = Member.builder()
                 .loginId(loginId)
+                .encryptedPwd("-----")
                 .email(oAuthInfoResponse.getEmail())
+                .name(oAuthInfoResponse.getName())
+                .birthDate(oAuthInfoResponse.getBirthday())
                 .gender(oAuthInfoResponse.getGender())
+                .phoneNumber(oAuthInfoResponse.getMobile())
+                .active(true) // 예시로 true로 설정
+                .loginType(true) // 예시로 true로 설정
+                .oAuthProvider(oAuthInfoResponse.getOAuthProvider())
+                .locationPermission(true) // 예시로 true로 설정
+                .callPermission(true) // 예시로 true로 설정
+                .cameraPermission(true) // 예시로 true로 설정
                 .build();
         return memberRepository.save(member);
     }
