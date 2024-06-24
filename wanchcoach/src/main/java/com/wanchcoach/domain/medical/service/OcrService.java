561ff2b03f32f0c10500a2fe5966aa2da9314a92
parkeg1223 <parkeg1223@gmail.com>
2024-06-24 22:15:24 +0900

    WC-846/refactor: for logic 변경

--- wanchcoach/src/main/java/com/wanchcoach/domain/medical/service/OcrService.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/medical/service/OcrService.java
@@ -12,7 +12,6 @@ import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.net.HttpURLConnection;
