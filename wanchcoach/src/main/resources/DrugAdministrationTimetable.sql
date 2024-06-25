f0e0c64e94b0e6b5272c2ce1eed99e5d76bedae5
parkeg1223 <parkeg1223@gmail.com>
2024-06-25 21:14:49 +0900

    WC-715/docs: base entity 속성 추가

--- wanchcoach/src/main/resources/DrugAdministrationTimetable.sql
+++ wanchcoach/src/main/resources/DrugAdministrationTimetable.sql
@@ -5,6 +5,8 @@ CREATE TABLE drug_administration_time (
                                           noon TIME NOT NULL,
                                           evening TIME NOT NULL,
                                           before_bed TIME NOT NULL,
+                                          CREATED_DATE DATETIME NOT NULL,
+                                          MODIFIED_DATE DATETIME NOT NULL,
                                           PRIMARY KEY (id),
                                           CONSTRAINT fk_member_id FOREIGN KEY (member_id) REFERENCES member (member_id)
 );
