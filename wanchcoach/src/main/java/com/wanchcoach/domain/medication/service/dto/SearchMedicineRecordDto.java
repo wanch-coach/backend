31f752026c65a2b18f0169718aae837579cd096b
marugy <gyulife7301@naver.com>
2024-06-26 01:31:27 +0900

    WC-887/ã7/feat/fix: SearchMedicineRecordDto

--- wanchcoach/src/main/java/com/wanchcoach/domain/medication/service/dto/SearchMedicineRecordDto.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/medication/service/dto/SearchMedicineRecordDto.java
@@ -3,6 +3,6 @@ package com.wanchcoach.domain.medication.service.dto;
 import java.time.LocalDateTime;
 
 public record SearchMedicineRecordDto(
-        LocalDateTime takenTime;
+        LocalDateTime takenTime
 ) {
 }
