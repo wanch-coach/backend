ecb4550194c0e2bcdb637fdb91912ace0ef7c7c1
marugy <gyulife7301@naver.com>
2024-06-27 00:42:40 +0900

    WC-889/89/feat: : 일별 복약 조회 api, drug Entity 수정

---
+++ wanchcoach/src/main/java/com/wanchcoach/domain/medication/controller/response/DailyPrescriptionInfo.java
@@ -0,0 +1,15 @@
+package com.wanchcoach.domain.medication.controller.response;
+
+import com.wanchcoach.domain.drug.controller.dto.response.SearchDrugsResponse;
+
+import java.util.List;
+
+public record DailyPrescriptionInfo(
+        Long prescriptionId,
+        String hospitalName,
+        String department,
+        int remains,
+        List<SearchDrugsResponse> drugs
+) {
+
+}
