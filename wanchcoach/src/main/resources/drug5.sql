e12c31d3140269ca05f79caa82aa7ddfaf20ace3
marugy <gyulife7301@naver.com>
2024-06-27 21:58:19 +0900

    chore: devide hospital_opening_hour.sql into 1.sql, 2.sql

---
+++ wanchcoach/src/main/resources/drug5.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c68d74f4b1ea1e5bff7583960a425e804aec1a2b0b50553619fb3dc303bbb37
+size 1308779
---
+++ wanchcoach/src/main/resources/drug_image.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad28e01610d18cee2055b716d5c1f58176c62b83b04801d42806fa860046a66a
+size 3906664
