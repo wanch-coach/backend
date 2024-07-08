be7adccde019d3c41f31d49dba13e8219fdd8a95
marugy <gyulife7301@naver.com>
2024-07-09 00:15:00 +0900

    feat: 복약 먹기 api request 값 날짜 추가 및 medicineRecord Entity 복약 날짜 추가

--- wanchcoach/src/main/java/com/wanchcoach/domain/medication/controller/request/TakingMedicineRequest.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/medication/controller/request/TakingMedicineRequest.java
@@ -1,7 +1,11 @@
 package com.wanchcoach.domain.medication.controller.request;
 
+import java.time.LocalDate;
+
 public record TakingMedicineRequest (
         Long familyId,
+
+        LocalDate takenDate,
         String time
 ){
 }
