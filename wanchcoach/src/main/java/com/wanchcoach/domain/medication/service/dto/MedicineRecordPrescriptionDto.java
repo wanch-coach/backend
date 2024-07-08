964558141c2c1dfbe78dcc911fdd415d53aa2680
marugy <gyulife7301@naver.com>
2024-07-09 02:03:10 +0900

    fix: MedicineRecordPrescriptionDto LocalDate

--- wanchcoach/src/main/java/com/wanchcoach/domain/medication/service/dto/MedicineRecordPrescriptionDto.java
+++ wanchcoach/src/main/java/com/wanchcoach/domain/medication/service/dto/MedicineRecordPrescriptionDto.java
@@ -1,13 +1,13 @@
 package com.wanchcoach.domain.medication.service.dto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public record MedicineRecordPrescriptionDto(
     Long hospitalId,
     String hospitalName,
     String department,
     Long prescriptionId,
-    LocalDateTime takenDay,
+    LocalDate takenDay,
     int time
 ) {
 }
